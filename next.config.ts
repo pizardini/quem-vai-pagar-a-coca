c47d849318416f55f996507248885a7be8b4f93e
Pietro Zardini <pizardini@gmail.com>
2026-07-23 11:50:50 -0300

    Configure static export for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+  basePath: isProd ? "/quem-vai-pagar-a-coca" : "",
+  assetPrefix: isProd ? "/quem-vai-pagar-a-coca/" : "",
 };
 
 export default nextConfig;
